6ab5e867f43c23cd8067e9a6ca67c59af41a3842
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-22 21:54:38 +0100

    shorter code little fix

--- routes/auth/local/index.js
+++ routes/auth/local/index.js
@@ -9,15 +9,15 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
-  Account.register( new Account({ username : req.body.username }), req.body.password, function(err, account) {
-      if (err) {
-          return res.render('register', { account : account });
-      }
 
-      passport.authenticate('local')(req, res, function () {
-        res.redirect('/');
-      });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  Account.register( new Account({ username }), password, (err, account) => {
+      if (err) return res.render('register', { account : account });
+      passport.authenticate('local')(req, res, () =>  res.redirect('/') );
   });
+
 });
 
 router.get('/login', function(req, res) {
